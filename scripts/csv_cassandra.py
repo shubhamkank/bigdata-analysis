f291fce37ce399e4c1991d09cb15a419ccb36c17
Shubham Kankaria <shubhamkank@gmail.com>
2016-01-31 00:27:59 +0530

    Insert Into Cassandra Script Added

--- scripts/csv_cassandra.py
+++ scripts/csv_cassandra.py
@@ -1,10 +1,24 @@
+import sys
 from cassandra.cluster import Cluster
 
-cluster = Cluster()
-session = cluster.connect('demo')
+INSERT_QUERY = "insert into airport_airline_departure (Origin, UniqueCarrier, AirlineID, DepDelayMinutes) values ('{}', '{}', '{}', {})"
+KEYSPACE = "aviation"
 
-session.execute("insert into table1 (name, id) values ('James', 10)")
+def main():
+    if len(sys.argv) != 2:
+        print 'usage: python insert_cassandra.py <input-file-path>'
 
-result = session.execute("select * from table1")
+    filename = sys.argv[1]
 
-print result
+    cluster = Cluster()
+    session = cluster.connect(KEYSPACE)
+
+    for line in open(filename):
+        line = line.rstrip()
+        row = line.split("\t")
+
+        insert_query = INSERT_QUERY.format(row[0], row[1], row[2], row[3])
+        session.execute(insert_query);
+
+if __name__ == '__main__':
+    main()
